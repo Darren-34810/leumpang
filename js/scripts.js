cdc1933edc5d9e3f84ad591ef311bc18900386f3
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-10-23 23:53:25 +0700

    Homepage structure completed, removed unnecessary pages

--- js/scripts.js
+++ js/scripts.js
@@ -67,14 +67,14 @@
 	var cardSlider = new Swiper('.card-slider', {
 		autoplay: {
             delay: 4000,
-            disableOnInteraction: false
+            disableOnInteraction: false,
 		},
         loop: true,
         navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev'
 		},
-		slidesPerView: 3,
+        slidesPerView: 2,
 		spaceBetween: 20,
         breakpoints: {
             // when window is <= 992px
